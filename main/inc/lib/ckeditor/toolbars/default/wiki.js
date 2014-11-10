a960485b28aa272281fa023fa357925ad85776bc
Imanol Losada <imanol.losada@gmail.com>
2014-11-10 15:19:51 -0500

    Add wiki default and extended toolbars - refs BT#8968

---
+++ main/inc/lib/ckeditor/toolbars/default/wiki.js
@@ -0,0 +1,21 @@
+CKEDITOR.editorConfig = function( config ) {
+    config.extraPlugins = 'button,maximize,toolbar,toolbarswitch';
+    config.toolbar_Basic =
+        [
+            ['Maximize','Save','NewPage','Templates','PageBreak','Preview','-','PasteText','-','Undo','Redo','-','SelectAll','-','Find'],
+            ['Wikilink','Link','Unlink','Anchor'],
+            ['Image','flvPlayer','Flash','EmbedMovies','YouTube','MP3','mimetex','asciimath','fckeditor_wiris_openFormulaEditor','fckeditor_wiris_openCAS'],
+            ['Table','Rule','Smiley','SpecialChar','googlemaps'],
+            ['FontFormat','FontName','FontSize'],
+            ['Bold','Italic','Underline'],
+            ['Subscript','Superscript','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyFull','-','OrderedList','UnorderedList','-','Outdent','Indent','-','TextColor','BGColor', 'SpellCheck'],
+            ['Source']
+        ];
+    config.toolbar = 'Basic';
+    config.smallToolbar = 'Basic';
+    config.allowedContent = true;
+};
+
+function CKeditor_OnComplete(ckEditorInstance) {
+
+};
