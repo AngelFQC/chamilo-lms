37a0b489734c9bca119dec4b3abbe418da060c95
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2014-11-11 13:55:51 -0500

    Set the toolbar for Agenda form - refs BT#8968

---
+++ main/inc/lib/ckeditor/toolbars/extended/agenda.js.php
@@ -0,0 +1,73 @@
+<?php
+/* For licensing terms, see /license.txt */
+/**
+ * CKEditor toolbar for Agenda form
+ * @author Angel Fernando Quiroz Campos <angel.quiroz@beeznest.com>
+ */
+header('Content-Type: application/x-javascript');
+
+require_once '../../../main_api.lib.php';
+
+if ((api_get_setting('allow_spellcheck') != 'true')) {
+    $SpellChecker = 'Scayt';
+} else {
+    $SpellChecker = '';
+}
+
+$extraPlugins = array(
+    'toolbarswitch',
+);
+
+$toolbarBasic = array(
+    array('Save', 'NewPage', 'Templates', '-', 'PasteFromWord'),
+    array('Undo', 'Redo'),
+    array('Link', 'Image', 'flvPlayer', 'Flash', 'MP3', 'TableOC', 'mimetex', 'asciimath', 'asciisvg'),
+    array('BulletedList', 'NumberedList', 'HorizontalRule'),
+    array('JustifyLeft', 'JustifyCenter'),
+    array('Format', 'Font', 'Bold', 'Italic', 'Underline', 'TextColor', 'BGColor', 'Source'),
+    array('Toolbarswitch')
+);
+
+$toolbarFull = array(
+    array('Save', 'NewPage', 'Templates', '-', 'Preview', 'Print'),
+    array('Cut', 'Copy', 'Paste', 'PasteText', 'PasteFromWord'),
+    array('Undo', 'Redo', '-', 'SelectAll', 'Find', '-', 'RemoveFormat'),
+    array('Link', 'Unlink', 'Anchor', 'Glossary'),
+    array('Image', 'imgmapPopup', 'flvPlayer', 'EmbedMovies', 'YouTube', 'Flash', 'MP3', 'googlemaps', 'Smiley', 'SpecialChar', 'insertHtml', 'mimetex', 'asciimath', 'asciisvg', 'fckeditor_wiris_openFormulaEditor', 'fckeditor_wiris_openCAS'),
+    '/',
+    array('TableOC', 'Table', 'TableInsertRowAfter', 'TableDeleteRows', 'TableInsertColumnAfter', 'TableDeleteColumns', 'TableInsertCellAfter', 'TableDeleteCells', 'TableMergeCells', 'TableHorizontalSplitCell', 'TableVerticalSplitCell', 'TableCellProp', '-', 'CreateDiv'),
+    array('BulletedList', 'NumberedList', 'HorizontalRule', '-', 'Outdent', 'Indent', 'Blockquote'),
+    array('JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'),
+    array('Bold', 'Italic', 'Underline', 'Strike', '-', 'Subscript', 'Superscript', '-', 'TextColor', 'BGColor'),
+    array($SpellChecker),
+    array('Style', 'Format', 'Font', 'FontSize'),
+    array('PageBreak', 'ShowBlocks', 'Source'),
+    array('Toolbarswitch')
+);
+?>
+CKEDITOR.editorConfig = function(config) {
+    config.extraPlugins = '<?php echo implode(',', $extraPlugins) ?>';
+    config.toolbar_Basic = <?php echo json_encode($toolbarBasic) ?>;
+    config.toolbar_Full = <?php echo json_encode($toolbarFull) ?>;
+    config.toolbar = 'Basic';
+    config.smallToolbar = 'Basic';
+    config.maximizedToolbar = 'Full';
+    config.allowedContent = true;
+
+    // Sets whether the toolbar can be collapsed/expanded or not.
+    // Possible values: true , false
+    config.toolbarCanCollapse = true;
+
+    // This option sets the location of the toolbar.
+    // Possible values: 'top' , 'bottom'
+    // config.toolbarLocation = 'top';
+
+    // Here new width and height of the editor may be set.
+    // Possible values, examples: 300 , '250' , '100%' , ...
+    // config.width = '100%';
+    // config.height = 400;
+};
+
+function CKeditor_OnComplete(ckEditorInstance) {
+
+}
