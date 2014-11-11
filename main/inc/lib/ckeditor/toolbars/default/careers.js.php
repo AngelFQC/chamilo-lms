b6f9f942e1a00b21365c678a59d3c74db783907f
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2014-11-11 15:40:13 -0500

    Set CKEditor toolbars to Careers form - refs BT#8968

---
+++ main/inc/lib/ckeditor/toolbars/default/careers.js.php
@@ -0,0 +1,54 @@
+<?php
+/* For licensing terms, see /license.txt */
+/**
+ * CKEditor toolbar for Careers form
+ * @author Angel Fernando Quiroz Campos <angel.quiroz@beeznest.com>
+ */
+header('Content-Type: application/x-javascript');
+
+require_once '../../../main_api.lib.php';
+
+if ((api_get_setting('allow_spellcheck') != 'true')) {
+    $SpellChecker = 'Scayt';
+} else {
+    $SpellChecker = '';
+}
+
+$extraPlugins = array(
+    'toolbarswitch',
+);
+
+$toolbarBasic = array(
+    array('Save','NewPage','Templates','-','PasteText'),
+	array('Undo','Redo'),
+	array('Link','Image','flvPlayer','Flash','MP3','TableOC','mimetex'),
+	array('BulletedList','NumberedList','HorizontalRule'),
+	array('JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'),
+	array('Format','Font','FontSize','Bold','Italic','Underline','TextColor','BGColor','Source'),
+	array('Toolbarswitch')
+);
+?>
+CKEDITOR.editorConfig = function(config) {
+    config.extraPlugins = '<?php echo implode(',', $extraPlugins) ?>';
+    config.toolbar_Basic = <?php echo json_encode($toolbarBasic) ?>;
+    config.toolbar = 'Basic';
+    config.smallToolbar = 'Basic';
+    config.allowedContent = true;
+
+    // Sets whether the toolbar can be collapsed/expanded or not.
+    // Possible values: true , false
+    // config.toolbarCanCollapse = true;
+
+    // This option sets the location of the toolbar.
+    // Possible values: 'top' , 'bottom'
+    // config.toolbarLocation = 'top';
+
+    // Here new width and height of the editor may be set.
+    // Possible values, examples: 300 , '250' , '100%' , ...
+    // config.width = '100%';
+    // config.height = 300;
+};
+
+function CKeditor_OnComplete(ckEditorInstance) {
+
+}
