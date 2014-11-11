31553eab212fe1d7141f919c8ccb44b015c0a8f1
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2014-11-11 15:29:28 -0500

    Set CKEditor toolbars to Assesments Introduction form - refs BT#8968

---
+++ main/inc/lib/ckeditor/toolbars/default/assessmentsintroduction.js.php
@@ -0,0 +1,57 @@
+<?php
+/* For licensing terms, see /license.txt */
+/**
+ * CKEditor toolbar for Assessments Introduction form
+ * @author Angel Fernando Quiroz Campos <angel.quiroz@beeznest.com>
+ */
+header('Content-Type: application/x-javascript');
+
+require_once '../../../main_api.lib.php';
+
+if ((api_get_setting('allow_spellcheck') != 'true')) {
+    $SpellChecker = 'Scayt';
+} else {
+    $SpellChecker = '';
+}
+
+$extraPlugins = array(
+    'toolbarswitch',
+);
+
+$toolbarBasic = array(
+    array('Save','Toolbarswitch','-','PasteFromWord','-','Undo','Redo'),
+    array('Link','Unlink','Anchor'),
+    array('Image','flvPlayer','Flash','EmbedMovies','YouTube','MP3','mimetex'),
+    array('Table','SpecialChar'),
+    array('NumberedList','BulletedList','-','Outdent','Indent','-','TextColor','BGColor'),
+    '/',
+    array('Style','FontFormat','FontName','FontSize'),
+    array('Bold','Italic','Underline'),
+    array('Subscript','Superscript','-','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'),
+    array('Source')
+);
+?>
+CKEDITOR.editorConfig = function(config) {
+    config.extraPlugins = '<?php echo implode(',', $extraPlugins) ?>';
+    config.toolbar_Basic = <?php echo json_encode($toolbarBasic) ?>;
+    config.toolbar = 'Basic';
+    config.smallToolbar = 'Basic';
+    config.allowedContent = true;
+
+    // Sets whether the toolbar can be collapsed/expanded or not.
+    // Possible values: true , false
+    // config.toolbarCanCollapse = true;
+
+    // This option sets the location of the toolbar.
+    // Possible values: 'top' , 'bottom'
+    // config.toolbarLocation = 'top';
+
+    // Here new width and height of the editor may be set.
+    // Possible values, examples: 300 , '250' , '100%' , ...
+    // config.width = '100%';
+    // config.height = 300;
+};
+
+function CKeditor_OnComplete(ckEditorInstance) {
+
+}
