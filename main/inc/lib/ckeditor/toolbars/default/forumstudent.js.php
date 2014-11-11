43c0cb9696825a5d77749f167baf36b78921a8c5
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2014-11-11 17:06:07 -0500

    Set CKEditor toolbars to Forum Student form - refs BT#8968

---
+++ main/inc/lib/ckeditor/toolbars/default/forumstudent.js.php
@@ -0,0 +1,49 @@
+<?php
+/* For licensing terms, see /license.txt */
+/**
+ * CKEditor toolbar for Forum form
+ * @author Angel Fernando Quiroz Campos <angel.quiroz@beeznest.com>
+ */
+header('Content-Type: application/x-javascript');
+
+$extraPlugins = array(
+    'toolbarswitch',
+);
+
+$toolbarBasic = array(
+    array('Save', 'Toolbarswitch', 'PasteFromWord', '-', 'Undo', 'Redo'),
+    array('Link', 'Unlink', 'Anchor'),
+    array('Image', 'flvPlayer', 'Flash', 'EmbedMovies', 'YouTube', 'MP3', 'mimetex'),
+    array('Table', 'SpecialChar'),
+    array('NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'TextColor', 'BGColor'),
+    '/',
+    array('Styles', 'Format', 'Font', 'FontSize'),
+    array('Bold', 'Italic', 'Underline'),
+    array('JustifyLeft', 'JustifyCenter', 'JustifyRight'),
+	array('ShowBlocks')
+);
+?>
+CKEDITOR.editorConfig = function(config) {
+    config.extraPlugins = '<?php echo implode(',', $extraPlugins) ?>';
+    config.toolbar_Basic = <?php echo json_encode($toolbarBasic) ?>;
+    config.toolbar = 'Basic';
+    config.smallToolbar = 'Basic';
+    config.allowedContent = true;
+
+    // Sets whether the toolbar can be collapsed/expanded or not.
+    // Possible values: true , false
+    // config.toolbarCanCollapse = true;
+
+    // This option sets the location of the toolbar.
+    // Possible values: 'top' , 'bottom'
+    // config.toolbarLocation = 'top';
+
+    // Here new width and height of the editor may be set.
+    // Possible values, examples: 300 , '250' , '100%' , ...
+    // config.width = '100%';
+    // config.height = 400;
+};
+
+function CKeditor_OnComplete(ckEditorInstance) {
+
+}
