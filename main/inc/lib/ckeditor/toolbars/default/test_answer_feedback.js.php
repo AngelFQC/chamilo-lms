f3bf82daa44d24eededfafdaeb62b85a77a6c057
Imanol Losada <imanol.losada@gmail.com>
2014-11-12 17:01:39 -0500

    Add test_answer_feedback default and extended .js.php toolbars - refs BT#8968

---
+++ main/inc/lib/ckeditor/toolbars/default/test_answer_feedback.js.php
@@ -0,0 +1,33 @@
+<?php
+/* For licensing terms, see /license.txt */
+/**
+ * CKEditor toolbar for Test Answer Feedback form
+ * @author Imanol Losada Oriol <imanol.losada@beeznest.com>
+ */
+header('Content-Type: application/x-javascript');
+
+require_once '../../../main_api.lib.php';
+
+$extraPlugins = array(
+    'toolbarswitch',
+);
+
+// This is the visible toolbar set when the editor has "normal" size.
+$toolbarBasic = array(
+	array('Link','Unlink','Bold','Italic','TextColor','BGColor','mimetex')
+);
+
+$config['ToolbarStartExpanded'] = false;
+?>
+
+CKEDITOR.editorConfig = function(config) {
+config.extraPlugins = '<?php echo implode(',', $extraPlugins) ?>';
+config.toolbar_Basic = <?php echo json_encode($toolbarBasic) ?>;
+config.toolbar = 'Basic';
+config.smallToolbar = 'Basic';
+config.allowedContent = true;
+};
+
+function CKeditor_OnComplete(ckEditorInstance) {
+
+}
---
+++ main/inc/lib/ckeditor/toolbars/extended/test_answer_feedback.js.php
@@ -0,0 +1,71 @@
+<?php
+/* For licensing terms, see /license.txt */
+/**
+ * CKEditor toolbar for Test Answer Feedback form
+ * @author Imanol Losada Oriol <imanol.losada@beeznest.com>
+ */
+header('Content-Type: application/x-javascript');
+
+require_once '../../../main_api.lib.php';
+
+// Hide/show SpellCheck button
+if ((api_get_setting('allow_spellcheck') == 'true')) {
+    $VSpellCheck='Scayt';
+}
+else{
+    $VSpellCheck='';
+}
+
+$extraPlugins = array(
+    'toolbarswitch',
+);
+
+// This is the visible toolbar set when the editor has "normal" size.
+$toolbarBasic = array(
+    array('NewPage','Templates','-','PasteFromWord'),
+    array('Undo','Redo'),
+    array('Link','Image','flvPlayer','Flash','MP3','TableOC','mimetex'),
+    array('BulletedList','NumberedList','HorizontalRule'),
+    array('JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'),
+	array('Abbr'),
+    array('Format','Font','FontSize','Bold','Italic','Underline','TextColor','BGColor','Source'),
+    array('Toolbarswitch')
+);
+
+// This is the visible toolbar set when the editor is maximized.
+// If it has not been defined, then the toolbar set for the "normal" size is used.
+$config['ToolbarSets']['Maximized'] = array(
+    array('NewPage','Templates','-','Preview','Print'),
+    array('Cut','Copy','Paste','PasteText','PasteFromWord'),
+    array('Undo','Redo','-','SelectAll','Find','-','RemoveFormat'),
+    array('Link','Unlink','Anchor','Glossary'),
+    array('Image','imgmapPopup','flvPlayer','EmbedMovies','YouTube','Flash','MP3','googlemaps','Smiley','SpecialChar','insertHtml','mimetex','fckeditor_wiris_openFormulaEditor','fckeditor_wiris_openCAS'),
+'/',
+    array('TableOC','Table','TableInsertRowAfter','TableDeleteRows','TableInsertColumnAfter','TableDeleteColumns','TableInsertCellAfter','TableDeleteCells','TableMergeCells','TableHorizontalSplitCell','TableVerticalSplitCell','TableCellProp','-','CreateDiv'),
+    array('BulletedList','NumberedList','HorizontalRule','-','Outdent','Indent','Blockquote'),
+    array('JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'),
+    array('Bold','Italic','Underline','Strike','-','Subscript','Superscript','-','TextColor','BGColor'),
+    array('Abbr'),
+	array($VSpellCheck),
+    array('Styles','Format','Font','FontSize'),
+    array('PageBreak','ShowBlocks','Source'),
+    array('Toolbarswitch')
+
+);
+
+$config['ToolbarStartExpanded'] = false;
+?>
+
+CKEDITOR.editorConfig = function(config) {
+config.extraPlugins = '<?php echo implode(',', $extraPlugins) ?>';
+config.toolbar_Basic = <?php echo json_encode($toolbarBasic) ?>;
+config.toolbar_Full = <?php echo json_encode($toolbarFull) ?>;
+config.toolbar = 'Basic';
+config.smallToolbar = 'Basic';
+config.maximizedToolbar = 'Full';
+config.allowedContent = true;
+};
+
+function CKeditor_OnComplete(ckEditorInstance) {
+
+}
