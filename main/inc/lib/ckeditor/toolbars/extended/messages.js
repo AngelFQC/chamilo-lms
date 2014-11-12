157fac251ca0f13a5028bdb534636e34c8467f5e
Imanol Losada <imanol.losada@gmail.com>
2014-11-12 08:48:55 -0500

    Add messages default and extended toolbars - refs BT#8968

---
+++ main/inc/lib/ckeditor/toolbars/extended/messages.js
@@ -0,0 +1,38 @@
+CKEDITOR.editorConfig = function( config ) {
+    config.extraPlugins = 'button,maximize,toolbar,toolbarswitch';
+    config.toolbar_Basic =
+        [
+            ['NewPage','-','PasteWord'],
+            ['Undo','Redo'],
+            ['Link','Image','flvPlayer','Flash','MP3','TableOC','mimetex','asciimath','asciisvg'],
+            ['UnorderedList','OrderedList','Rule'],
+            ['JustifyLeft','JustifyCenter'],
+            ['FontFormat','FontName','Bold','Italic','Underline','TextColor','BGColor'],
+            ['Toolbarswitch']
+        ];
+    config.toolbar_Full =
+        [
+            ['NewPage','Templates','-','Preview','Print'],
+            ['Cut','Copy','Paste','PasteText','PasteWord'],
+            ['Undo','Redo','-','SelectAll','Find','-','RemoveFormat'],
+            ['Link','Unlink','Anchor','Glossary'],
+            ['Image','imgmapPopup','flvPlayer','EmbedMovies','YouTube','Flash','MP3','googlemaps','Smiley','SpecialChar','insertHtml','mimetex','asciimath','asciisvg','fckeditor_wiris_openFormulaEditor','fckeditor_wiris_openCAS'],
+            '/',
+            ['TableOC','Table','TableInsertRowAfter','TableDeleteRows','TableInsertColumnAfter','TableDeleteColumns','TableInsertCellAfter','TableDeleteCells','TableMergeCells','TableHorizontalSplitCell','TableVerticalSplitCell','TableCellProp','-','CreateDiv'],
+            ['UnorderedList','OrderedList','Rule','-','Outdent','Indent','Blockquote'],
+            ['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
+            ['Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript','-','TextColor','BGColor'],
+            ['SpellCheck'],
+            ['Style','FontFormat','FontName','FontSize'],
+            ['PageBreak','ShowBlocks','Source'],
+            ['Toolbarswitch']
+        ];
+    config.toolbar = 'Basic';
+    config.smallToolbar = 'Basic';
+    config.maximizedToolbar = 'Full';
+    config.allowedContent = true;
+};
+
+function CKeditor_OnComplete(ckEditorInstance) {
+
+};
