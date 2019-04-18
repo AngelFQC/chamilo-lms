de04cca16effb729f6e79447857db2a3e50e09f3
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2019-04-18 17:54:42 -0500

    BGO: Require migration class - refs BT#15387

--- tests/migrations/bgo/migrate.php
+++ tests/migrations/bgo/migrate.php
@@ -2,7 +2,7 @@
 /* For licensing terms, see /license.txt */
 
 require_once __DIR__.'/../../../main/inc/global.inc.php';
-
+require_once __DIR__.'/BgoMigration.php';
 require_once __DIR__.'/config.php';
 
 $migrate = new BgoMigration($adminId, $oldPortalPath);
