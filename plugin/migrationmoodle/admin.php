0fd68f868110131401122334090ed4812c5b5c18
Angel Fernando Quiroz Campos <angelfqc.18@gmail.com>
2019-10-30 18:03:20 -0500

    MigrationMoodle: Allow execute course categories migration - refs BT#15992

--- plugin/migrationmoodle/admin.php
+++ plugin/migrationmoodle/admin.php
@@ -1,9 +1,40 @@
 <?php
 /* For licensing terms, see /license.txt */
 
+use Chamilo\PluginBundle\MigrationMoodle\Task\CourseCategoriesTask;
 use Chamilo\PluginBundle\MigrationMoodle\Task\UsersTask;
 
 require_once __DIR__.'/../../main/inc/global.inc.php';
 
-$usersMigration = new UsersTask();
-$usersMigration->execute();
+$action = $_GET['action'] ?? '';
+
+$selfUrl = api_get_self();
+
+$actionNames = [
+    'users' => 'Users',
+    'course_categories' => 'Course categories',
+];
+
+foreach ($actionNames as $actionName => $actionTitle) {
+    echo '<p>';
+    echo '<a href="'.$selfUrl.'?action='.$actionName.'">'.$actionTitle.'</a>';
+    echo '</p>';
+}
+
+if (!empty($action)) {
+    echo '<h3>'.$actionNames[$action].'</h3>';
+
+    switch ($action) {
+        case 'users':
+            $usersMigration = new UsersTask();
+            $usersMigration->execute();
+            break;
+        case 'course_categories':
+            $courseCategoriesMigration = new CourseCategoriesTask();
+            $courseCategoriesMigration->execute();
+            break;
+    }
+}
+
+
+
